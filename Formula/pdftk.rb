38778928986b2dee6237f1d0842e4fde1d0243cc
William Theaker <williamtheaker@users.noreply.github.com>
2022-05-25 09:55:29 -0400

    Download pdftk over TLS

--- Formula/pdftk.rb
+++ Formula/pdftk.rb
@@ -13,8 +13,8 @@
 class Pdftk < Formula
   version '2.02'
   FILENAME = "pdftk_server-#{version}-mac_osx-10.11-setup.pkg"
-  homepage 'http://www.pdflabs.com/tools/pdftk-server'
-  url "http://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/#{FILENAME}"
+  homepage 'https://www.pdflabs.com/tools/pdftk-server'
+  url "https://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/#{FILENAME}"
   sha256 'c33cf95151e477953cd57c1ea9c99ebdc29d75f4c9af0d5f947b385995750b0c'
 
   def install
