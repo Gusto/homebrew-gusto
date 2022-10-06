7f60361fd8880c3c27a4a1866648c63401a0f642
Ngan Pham <ngan@users.noreply.github.com>
2022-10-05 18:26:55 -0700

    Remove comment

--- Formula/wait-for-it.rb
+++ Formula/wait-for-it.rb
@@ -1,14 +1,6 @@
 # typed: false
 # frozen_string_literal: true
 
-# Previously, pdftk was in homebrew-cask, but it was removed because its installer changed
-# /usr/local permissions which caused issues with brew update
-#
-# See https://github.com/Homebrew/homebrew-cask/issues/7707 for more details
-#
-# This formula was sourced from https://github.com/zph/homebrew-cervezas/blob/master/pdftk.rb
-# The difference is this is a formlua, not a cask. It also extracts the contents of the pkg
-# to install directly, rather than relaying on the package
 class WaitForIt < Formula
   desc "CLI to test if a given TCP host/port are available"
   FILENAME = "wait-for-it.sh"
