6d3fcc3aed73586d493541b8fc4e95ca11e3bb7a
Josh Nichols <josh.nichols@gusto.com>
2022-10-06 12:03:45 -0400

    Change to be a head-only formula

--- Formula/wait-for-it.rb
+++ Formula/wait-for-it.rb
@@ -3,11 +3,10 @@
 
 class WaitForIt < Formula
   desc "CLI to test if a given TCP host/port are available"
-  FILENAME = "wait-for-it.sh"
   homepage "https://github.com/vishnubob/wait-for-it"
-  url "https://raw.githubusercontent.com/vishnubob/wait-for-it/master/#{FILENAME}"
-  version "master"
   sha256 "b7a04f38de1e51e7455ecf63151c8c7e405bd2d45a2d4e16f6419db737a125d6"
+
+  head 'https://github.com/vishnubob/wait-for-it.git'
 
   def install
     bin.install "wait-for-it.sh" => "wait-for-it"
