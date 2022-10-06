bda17ebf0710b9304bedff72d83772291d3129c9
Josh Nichols <josh.nichols@gusto.com>
2022-10-06 13:11:57 -0400

    Try replacing brew test-bot --only-tap-syntax with each step it does, and add except=specs

--- Formula/wait-for-it.rb
+++ Formula/wait-for-it.rb
@@ -1,11 +1,12 @@
 # typed: false
 # frozen_string_literal: true
 
-class WaitForItHeadOnly < Formula
+class WaitForIt < Formula
   desc "CLI to test if a given TCP host/port are available"
   homepage "https://github.com/vishnubob/wait-for-it"
 
-  head "https://github.com/vishnubob/wait-for-it.git"
+  # choose the recent revision so we can have a stable revision
+  head "https://github.com/vishnubob/wait-for-it.git", revision: "81b1373f17855a4dc21156cfe1694c31d7d1792e"
 
   def install
     bin.install "wait-for-it.sh" => "wait-for-it"
