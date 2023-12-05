cfafbca727bf78fade4a7258ebdcf8f18ca91a35
Josh Nichols <josh.nichols@gusto.com>
2023-12-05 14:38:35 -0500

    Add packs-rs

---
+++ Formula/packs.rb
@@ -0,0 +1,14 @@
+# typed: false
+# frozen_string_literal: true
+
+class Packs < Formula
+  desc "A pure Rust implementation of packwerk, a gradual modularization tool for Ruby"
+  homepage "https://github.com/alexevanczuk/packs"
+  url "https://github.com/alexevanczuk/packs/releases/download/v0.1.80/packs-mac.tar.gz"
+  sha256 "f7b1dd465b8f79b67dd83e5cf0dc868a24fdc028c8ce219138e1775a8afa7239"
+  license "MIT"
+
+  def install
+    bin.install "packs" => "pks"
+  end
+end
