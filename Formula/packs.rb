c517a07e84ba985358810022adbc842b35aff10f
Joey Schoblaska <joey.schoblaska@gmail.com>
2024-01-19 10:35:37 -0600

    Update Formula/packs.rb
    Co-authored-by: Josh Nichols <joshua.nichols@gmail.com>

--- Formula/packs.rb
+++ Formula/packs.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 class Packs < Formula
-  desc "A pure Rust implementation of packwerk, a gradual modularization tool for Ruby"
+  desc "Pure Rust implementation of packwerk, a gradual modularization tool for Ruby"
   homepage "https://github.com/alexevanczuk/packs"
   url "https://github.com/alexevanczuk/packs/releases/download/v0.1.85/packs-mac.tar.gz"
   sha256 "f7b1dd465b8f79b67dd83e5cf0dc868a24fdc028c8ce219138e1775a8afa7239"
