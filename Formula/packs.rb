5a3b38f1c5dd58e0338a622768bba7a612d223c0
Joey Schoblaska <joey.schoblaska@gusto.com>
2024-01-19 10:08:50 -0600

    upgrade packs formula (from 0.1.80 to 0.1.85)

--- Formula/packs.rb
+++ Formula/packs.rb
@@ -4,7 +4,7 @@
 class Packs < Formula
   desc "A pure Rust implementation of packwerk, a gradual modularization tool for Ruby"
   homepage "https://github.com/alexevanczuk/packs"
-  url "https://github.com/alexevanczuk/packs/releases/download/v0.1.80/packs-mac.tar.gz"
+  url "https://github.com/alexevanczuk/packs/releases/download/v0.1.85/packs-mac.tar.gz"
   sha256 "f7b1dd465b8f79b67dd83e5cf0dc868a24fdc028c8ce219138e1775a8afa7239"
   license "MIT"
 
