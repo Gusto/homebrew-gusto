ca20b82dcd6c58af16a305b985a2ad7cb9cc0ed7
Joey Schoblaska <joey.schoblaska@gusto.com>
2024-01-19 10:51:10 -0600

    update packs formula sha256

--- Formula/packs.rb
+++ Formula/packs.rb
@@ -5,7 +5,7 @@ class Packs < Formula
   desc "Pure Rust implementation of packwerk, a gradual modularization tool for Ruby"
   homepage "https://github.com/alexevanczuk/packs"
   url "https://github.com/alexevanczuk/packs/releases/download/v0.1.85/packs-mac.tar.gz"
-  sha256 "f7b1dd465b8f79b67dd83e5cf0dc868a24fdc028c8ce219138e1775a8afa7239"
+  sha256 "6258fc183c1f71f24007dac6deffe58cbf18d40120ec99b937ce97f000fff50d"
   license "MIT"
 
   def install
