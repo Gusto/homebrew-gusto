4b1ceda743ec2ebe0d12505e1e26a2d48c09b4a4
Perry Hertler <perry.hertler@gusto.com>
2024-04-02 11:15:25 -0500

    pks v0.1.91 (#22)

--- Formula/packs.rb
+++ Formula/packs.rb
@@ -4,8 +4,8 @@
 class Packs < Formula
   desc "Pure Rust implementation of packwerk, a gradual modularization tool for Ruby"
   homepage "https://github.com/alexevanczuk/packs"
-  url "https://github.com/alexevanczuk/packs/releases/download/v0.1.90/packs-mac.tar.gz"
-  sha256 "f955dc0c5102baddfba9861fa7f77e88355603095c8f365b77c3471c2854aff9"
+  url "https://github.com/alexevanczuk/packs/releases/download/v0.1.91/packs-mac.tar.gz"
+  sha256 "06a06845bd0bb66c6cc86b3103d949ca424b2dd268ee5d4e5c9c8f0693648366"
   license "MIT"
 
   def install
