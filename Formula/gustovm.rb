b66f46bb8416d08942aa859d83cd0e2624942c52
Nicolas Lara Fonseca <nicolas.larafonseca@gusto.com>
2024-04-18 09:06:49 -0700

    pin colima version

--- Formula/gustovm.rb
+++ Formula/gustovm.rb
@@ -4,7 +4,8 @@ class Gustovm < Formula
       tag:      "v0.6.8",
       revision: "9b0809d0ed9ad3ff1e57c405f27324e6298ca04f"
 
-  depends_on "colima"
+  depends_on "go" => :build
+  depends_on "lima"
   depends_on "docker"
   depends_on "docker-credential-helper-ecr"
   depends_on "docker-compose" 
@@ -12,7 +13,13 @@ class Gustovm < Formula
   depends_on "docker-credential-helper"
 
   def install
-    bin.install_symlink "/opt/homebrew/opt/colima/bin/colima" => "gustovm"
+    project = "github.com/abiosoft/colima"
+    ldflags = %W[
+      -s -w
+      -X #{project}/config.appVersion=#{version}
+      -X #{project}/config.revision=#{Utils.git_head}
+    ]
+    system "go", "build", *std_go_args(ldflags:), "./cmd/colima"
   end
 
   service do
