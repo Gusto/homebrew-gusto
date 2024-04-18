db87f756c496b961eb6bdf2b219b9e0af268500c
Nicolas Lara Fonseca <nicolas.larafonseca@gusto.com>
2024-04-18 09:31:31 -0700

    add readme link

--- Formula/gustovm.rb
+++ Formula/gustovm.rb
@@ -1,16 +1,20 @@
 class Gustovm < Formula
   desc "Gusto's container runtime"
+  desc "Container runtimes on MacOS (and Linux) with minimal setup"
+  homepage "https://github.com/abiosoft/colima/blob/main/README.md"
   url "https://github.com/abiosoft/colima.git",
       tag:      "v0.6.8",
       revision: "9b0809d0ed9ad3ff1e57c405f27324e6298ca04f"
+  license "MIT"
+  head "https://github.com/abiosoft/colima.git", branch: "main"
 
   depends_on "go" => :build
-  depends_on "lima"
   depends_on "docker"
   depends_on "docker-credential-helper-ecr"
   depends_on "docker-compose" 
   depends_on "docker-buildx"
   depends_on "docker-credential-helper"
+  depends_on "lima"
 
   def install
     project = "github.com/abiosoft/colima"
