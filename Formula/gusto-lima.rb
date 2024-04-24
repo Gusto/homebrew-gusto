96817e6c2ce35b54ea7a7f3a6b252711e40f4029
Nicolas Lara Fonseca <nicolas.larafonseca@gusto.com>
2024-04-24 11:52:09 -0700

    extra space

--- Formula/gusto-lima.rb
+++ Formula/gusto-lima.rb
@@ -2,7 +2,7 @@ class GustoLima < Formula
   desc "Gusto's opinionated colima profile"
   homepage "https://github.com/abiosoft/colima/blob/main/README.md"
   url "https://github.com/abiosoft/colima.git",
-      tag:      "v0.1.0"
+    tag:      "v0.1.0"
   license "MIT"
   head "https://github.com/abiosoft/colima.git", branch: "main"
 
